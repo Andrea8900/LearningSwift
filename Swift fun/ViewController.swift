06447ad9be61f5f9c4fcbe938e007a45b802d442
Andreas Riebeling Rasmussen <Andrea8900@hotmail.com>
2017-11-14 17:53:26 +0100

    Initial Commit

---
+++ Swift fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift fun
+//
+//  Created by Andreas Rasmussen on 14/11/2017.
+//  Copyright © 2017 Andreas Rasmussen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
